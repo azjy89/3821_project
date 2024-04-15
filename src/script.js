7f1048ce280f7e8dc3c93ad747e58206ae3fd952
Brandon Sirimat <bsirimat@outlook.com>
2024-04-16 00:12:42 +1000

    implemented ability to add and move nodes

--- src/script.js
+++ src/script.js
@@ -26,3 +26,74 @@ function OnMouseUp(event){
   _dragElement=null;
 }
 
+var svg = d3.select("svg");
+width = svg.attr("width");
+height = svg.attr("height");
+
+var margin = { top: 20, right: 20, bottom: 20, left: 20 }; 
+var boundaryWidth = width - margin.left - margin.right;
+var boundaryHeight = height - margin.top - margin.bottom;
+
+var nodes = [],
+nodeId = 0;
+
+var simulation = d3.forceSimulation(nodes)
+.force("charge", d3.forceManyBody().strength(-2))
+.force("center", d3.forceCenter(width / 2, height / 2))
+.on("tick", ticked);
+
+function dragstarted(event, d) {
+  if (!event.active) simulation.alphaTarget(0.3).restart();
+    d.fx = d.x;
+    d.fy = d.y;
+}
+
+function dragged(event, d) {
+  d.fx = event.x;
+  d.fy = event.y;
+}
+
+function dragended(event, d) {
+if (!event.active) simulation.alphaTarget(0);
+  d.fx = null;
+  d.fy = null;
+}
+
+function ticked() {
+var u = svg.selectAll('.node')
+    .data(nodes, function(d) { return d.id; });
+
+var uEnter = u.enter()
+    .append('g')
+    .attr('class', 'node')
+    .call(d3.drag()
+        .on("start", dragstarted)
+        .on("drag", dragged)
+        .on("end", dragended));
+
+uEnter.append('circle')
+    .attr('r', 30);
+
+uEnter.append('text')
+  .text(function(d) { return d.id; })
+  .attr('x', -17.5)
+  .attr('y', 3);
+
+uEnter.merge(u)
+  .attr('transform', function(d) {
+    return 'translate(' + d.x + ',' + d.y + ')';
+  });
+}
+
+function addNodes() {
+var count = parseInt(document.getElementById("node-count").value);
+  for (var i = 0; i < count; i++) {
+    nodes.push({
+      id: "Node" + (++nodeId),
+      x: Math.random() * 1000,
+      y: Math.random() * 1000
+    });
+  }
+  simulation.nodes(nodes);
+  simulation.alphaTarget(0.3).restart();
+}
